ae290d055eb9fc538cd74f9e291cdd92d93fb0b1
Zefirka <161130791+DaoDeDzin123@users.noreply.github.com>
2025-03-12 17:10:43 +0700

    Update main.py

--- НПК/main.py
+++ НПК/main.py
@@ -4,9 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-# Пример: Генерация случайных данных
-X = np.random.rand(100, 1) * 10  # 100 случайных значений от 0 до 10
-y = 2.5 * X + np.random.randn(100, 1)  # y зависит от X с добавлением шума
+X = np.random.rand(100, 1) * 10 
+y = 2.5 * X + np.random.randn(100, 1) 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
